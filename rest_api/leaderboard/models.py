e238f85eac6c464b42b4ee688253dc45e211b39c
Smart School Councils <jack@involver.org.uk>
2021-01-19 23:33:44 +0000

    Added trending questions

--- rest_api/leaderboard/models.py
+++ rest_api/leaderboard/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django_mysql.models import JSONField
+import hashlib
 
 # Create your models here.
 
@@ -15,3 +17,48 @@ class ClassMeeting(models.Model):
 	date = models.DateField(null=True, blank=True)
 	school = models.ForeignKey(School, on_delete=models.CASCADE, null=True, blank=True)
 
+class WordDict(models.Model):
+	question = models.CharField(max_length=1000, default="")
+	wd = JSONField()
+	cms = models.ManyToManyField(ClassMeeting)
+	date = models.DateField(null=True, blank=True)
+
+	def to_py(self, value):
+		if value == "":
+			return None
+
+		try:
+			if isinstance(value, str):
+				return json.loads(value)
+		except ValueError:
+			pass
+		return value
+
+	def from_db_value(self, value):
+		return self.to_py(value)
+
+	def to_db_string(self, value):
+		if value == "":
+			return None
+		if isinstance(value, dict):
+			value = json.dumps(value, cls=DjangoJSONEncoder)
+		return value
+
+class MD5_Hash(models.Model):
+	md5 = models.CharField()
+
+	def same_hash():
+		BUF_SIZE = 65536
+		md5 = hashlib.md5()
+
+		with open('/home/ssc/SmartSchoolCouncils/rest_api/SSC-restapi/rest_api/leaderboard/csv_files/school_questions.csv') as f:
+			while True:
+				data = f.read(BUF_SIZE)
+				if not data:
+					break
+				md5.update(data)
+
+		if str(md5) == str(self.md5):
+			return True
+		else:
+			return False
--- rest_api/leaderboard/views.py
+++ rest_api/leaderboard/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.models import User, Group
+from django.db.models import Count
 from rest_framework import viewsets
 from rest_framework import permissions
-from leaderboard.serializers import UserSerializer, GroupSerializer, SchoolSerializer
+from leaderboard.serializers import UserSerializer, GroupSerializer, SchoolSerializer, ClassMeetingSerializer
 from leaderboard.models import School, ClassMeeting
 import pandas as pd
 import datetime
+import hashlib
+
 
 def load_csv():
 	df = pd.read_csv(r'/home/ssc/SmartSchoolCouncils/rest_api/SSC-restapi/rest_api/leaderboard/csv_files/school_questions.csv')
@@ -118,6 +121,8 @@ def load_csv():
 				temp_school.month_cm = 0
 				temp_school.save()
 
+	WordDict.objects.all().delete()
+
 	# Load the Values
 
 	for index, row in df.iterrows():
@@ -129,6 +134,7 @@ def load_csv():
 		date = row['Session Date'].split('/')
 		question = row['Question']
 
+		#Validate that row contains school
 
 		if school_name == "" or school_name == "Coronavirus Daily Debates" or school_name == None or school_name == "test":
 			continue
@@ -137,6 +143,8 @@ def load_csv():
 			continue
 
 		school_name = school_name.upper()
+
+		#Check if school already exists
 
 		if not School.objects.filter(name=school_name).exists():
 			temp_school = School.objects.create(name=school_name, total_cm=0, year_cm=0, term_cm=0, month_cm=0, first_post=today)
@@ -147,9 +155,36 @@ def load_csv():
 		temp_school = School.objects.get(name=school_name)
 
 
-		if not ClassMeeting.objects.filter(school=temp_school, date=date_field, question=question).exists():
-			temp_class = ClassMeeting.objects.create(school=temp_school, date=date_field, question=question)
-			temp_class.save()
+		#Check if CM already exists
+
+		if not ClassMeeting.objects.filter(school=temp_school, date=date_field, question=question).exists():	#Create Question Map
+
+			if date_field > today-timedelta(days=60):
+
+				word_count = {}
+				question = question.rstrip().split(' ')
+
+				for word in question:
+					if word in word_count:
+						word_count[word] += 1
+					else:
+						word_count[word] = 1
+
+				wordDict = ''
+
+				#Check if worddict exists already/make one
+				if WordDict.objects.filter(wd=word_count).exists():
+					wordDict = WordDict.objects.get(wd=word_count)
+				else:
+					wordDict = WordDict.objects.create(wd=word_count, question=question)
+				wordDict.save()
+
+				temp_class = ClassMeeting.objects.create(school=temp_school, date=date_field, question=question)
+				temp_class.save()
+
+
+		
+			#Update school stats for CM
 
 			temp_school.total_cm += 1
 
@@ -163,6 +198,24 @@ def load_csv():
 				temp_school.month_cm += 1
 			
 			temp_school.save()
+
+def check_hash():
+	md5 = ''
+	if len(MD5_Hash.objects.all()) > 0:
+		md5 = MD5_Hash.objects.get(id=1)
+	else:
+
+		with open('/home/ssc/SmartSchoolCouncils/rest_api/SSC-restapi/rest_api/leaderboard/csv_files/school_questions.csv') as f:
+			while True:
+				data = f.read(BUF_SIZE)
+				if not data:
+					break
+				md5.update(data)
+		
+			md5 = MD5_Hash.objects.create(md5=md5)
+			md5.save()
+
+	return md5.same_hash
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -184,7 +237,9 @@ class GroupViewSet(viewsets.ModelViewSet):
 
 class SchoolViewSet(viewsets.ModelViewSet):
 
-	load_csv()
+	if !check_hash:
+		load_csv()
+	
 	queryset = School.objects.all()
 	serializer_class = SchoolSerializer
 	permission_classes = [permissions.AllowAny]
@@ -192,8 +247,10 @@ class SchoolViewSet(viewsets.ModelViewSet):
 
 class TopAllTimeViewSet(viewsets.ModelViewSet):
 	
-	load_csv()
-	queryset = School.objects.order_by('-total_cm')
+	if !check_hash:
+		load_csv()
+	
+	queryset = School.objects.order_by('-total_cm')[:10]
 
 	serializer_class = SchoolSerializer
 
@@ -201,34 +258,54 @@ class TopAllTimeViewSet(viewsets.ModelViewSet):
 
 
 class TopYearViewSet(viewsets.ModelViewSet):
-	load_csv()
-	queryset = School.objects.order_by('-year_cm')
+
+	if !check_hash:
+		load_csv()
+
+	queryset = School.objects.order_by('-year_cm')[:10]
 
 	serializer_class = SchoolSerializer
 	
 	permission_classes = [permissions.AllowAny]
 
 class TopTermViewSet(viewsets.ModelViewSet):
-	load_csv()
-	queryset = School.objects.order_by('-term_cm')
+	if !check_hash:
+		load_csv()
+	
+	queryset = School.objects.order_by('-term_cm')[:10]
 
 	serializer_class = SchoolSerializer
 	
 	permission_classes = [permissions.AllowAny]
 
 class TopMonthViewSet(viewsets.ModelViewSet):
-	load_csv()
-	queryset = School.objects.order_by('-month_cm')
+	if !check_hash:
+		load_csv()
+
+	queryset = School.objects.order_by('-month_cm')[:10]
 
 	serializer_class = SchoolSerializer
 	
 	permission_classes = [permissions.AllowAny]
 
 class MostRecentQViewSet(viewsets.ModelViewSet):
-	load_csv()
-	queryset = ClassMeeting.objects.order_by('-date')
+	if !check_hash:
+		load_csv()
+
+	queryset = ClassMeeting.objects.order_by('-date')[:10]
 
 	serializer_class = ClassMeetingSerializer
 
 	permission_classes = [permissions.AllowAny]
+
+class TopTrendingCMQViewSet(viewsets.ModelViewSet):
+	if !check_hash:
+		load_csv()
+
+	queryset = WordDict.objects.annotate(cm_count=Count('cms')).order_by('-cm_count')[:10]
+	serializer_class = WordDictSerializer
+	permission_classes = [permissions.AllowAny]
+
+	
+	
 
